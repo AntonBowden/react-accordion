8badd173229c9876df5b500c6626d3ce814b6558
Anton Bowden <bowden.anton@gmail.com>
2017-08-08 13:29:01 +0100

    add toggle functionality

--- src/Accordion.js
+++ src/Accordion.js
@@ -2,17 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Accordion.css';
 
+
+const styles = {
+  active: {
+    display: 'inherit'
+  },
+  inactive: {
+    display: 'none'
+  }
+}
+
 class Accordion extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      active: false
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      active: !this.state.active
+    });
+  }
+
   render() {
+    const stateStyle = this.state.active ? styles.active : styles.inactive;
     return (
       <div className="Accordion">
         <section>
-          <h2>{this.props.heading}</h2>
-          <p>{this.props.content}</p>
+          <h2 onClick={ this.toggle }> {this.props.heading} </h2>
+          <p style={ stateStyle }> {this.props.content} </p>
         </section>
       </div>
     );
   }
 }
+
+// Accordion.propTypes = {
+//   heading: React.propTypes.string.isRequired,
+//   content: React.propTypes.string.isRequired
+// }
 
 export default Accordion;
